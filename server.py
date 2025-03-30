b9c8c4d422fd3903c82bdab045d56488a96951de
Andrew Beatty <andrew.beatty@gmit.ie>
2025-03-30 20:41:08 +0100

    allow cors

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 from flask import Flask, jsonify, request, abort
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app) # allow CORS for all domains on all routes.
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 from bookDAO import bookDAO
 
 app = Flask(__name__, static_url_path='', static_folder='.')
